3f8f6f3b5773975efc8e5fd838fe7bfa50c65490
carlos-godoy720 <bit.krypton@protonmail.com>
2022-08-06 19:29:31 -0400

    added page folders & about page

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
 // test sheet
 // https://script.google.com/macros/s/AKfycbwH5wiLI_ZhpCgJgJvLh1Bs976ssEqC8svUXdkuYGOewKFQ1Ts/exec
 
+const scriptURL = 'https://script.google.com/macros/s/AKfycbwH5wiLI_ZhpCgJgJvLh1Bs976ssEqC8svUXdkuYGOewKFQ1Ts/exec'
+const form = document.querySelector('form')
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(response => console.log('Success!', response))
+        .then(() => {
+            form.reset()
+        })
+        .catch(error => console.error('Error!', error.message))
+})
+
 function myFunction() {
     let x = document.getElementById("myTopnav");
     if (x.className === "topnav") {
